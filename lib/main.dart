4b1335cd4f438c267910ee5c45c98da38d0f8163
Mark Mamdouh <mark.mamdouh67@yahoo.com>
2024-09-14 17:26:27 +0300

    made noteView and changed theme to black

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: NotesView.routeName,
+      routes: {
+        NotesView.routeName: (context) => NotesView(),
+      },
+      theme: ThemeData(brightness: Brightness.dark),
+    );
+  }
+}
---
+++ lib/views/notes_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+  static const String routeName = "notesView";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
